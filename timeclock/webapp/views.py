08f89f539c00647617c021b2f650e1e0c2a5fb17
Mike Jarrett <mike.d.jarrett@gmail.com>
2015-03-15 08:20:04 +0000

    Add basic reports view for logged in user

--- timeclock/webapp/views.py
+++ timeclock/webapp/views.py
@@ -1,38 +1,42 @@
 # -*- coding: utf-8 -*-
+
 from flask import render_template, redirect
-from flask.ext.login import login_user
+from flask.ext.login import current_user, login_required, login_user
 
 from webapp import app
 from webapp.forms import LoginForm
 
-from logic.controller import UserController
+from logic import controller as logic
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Miguel'}
-    return render_template('index.html', title='Welcome', user=user)
+@app.route('/punches')
+@login_required
+def punches():
+    punches = logic.PunchController().get_user_punches_by_range(
+        current_user)
+    return render_template('reports.html', title='Reports', punches=punches)
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello monk butts!'
-
-
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        import ipdb; ipdb.set_trace()
-        controller = UserController()
+        controller = logic.UserController()
         user, validated = controller.validate_username_and_password(
             form.username.data, form.password.data)
 
         if validated:
-            login_user(user)
-            return redirect('/hello')
+            login_user(user, remember=True)
+            return redirect('/punches')
         else:
             return redirect('/index')
 
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/index')
