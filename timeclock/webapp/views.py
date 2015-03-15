8a7763690143587157d1a654e4c8e3dd5c189bd9
Mike Jarrett <mike.d.jarrett@gmail.com>
2015-03-15 11:19:10 +0000

    Add more views to webapp

--- timeclock/webapp/views.py
+++ timeclock/webapp/views.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, redirect
-from flask.ext.login import current_user, login_required, login_user
+from flask import render_template, redirect, request
+from flask.ext.login import (
+    current_user, login_required, login_user, logout_user
+)
 
 from webapp import app
 from webapp.forms import LoginForm
 
 from logic import controller as logic
-
-
-@app.route('/punches')
-@login_required
-def punches():
-    punches = logic.PunchController().get_user_punches_by_range(
-        current_user)
-    return render_template('reports.html', title='Reports', punches=punches)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -36,7 +30,21 @@ def login():
 
 
 @app.route("/logout")
-@login_required
 def logout():
     logout_user()
-    return redirect('/index')
+    return redirect('/')
+
+
+@app.route('/punches')
+@login_required
+def punches():
+    punches = logic.PunchController().get_user_punches_by_range(
+        current_user)
+    return render_template('reports.html', title='Reports', punches=punches)
+
+
+@app.route('/users')
+@login_required
+def users():
+    users = logic.UserController().get_users()
+    return render_template('users.html', title='Users', users=users)
