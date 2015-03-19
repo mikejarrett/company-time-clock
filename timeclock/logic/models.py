647fd52a4bf8356ea43bf3fc5178a1926933bd28
Mike Jarrett <mike.jarrett@hogarthww.com>
2015-03-19 08:57:23 +0000

    Add serializeable property to models

--- timeclock/logic/models.py
+++ timeclock/logic/models.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import (
     Column, DateTime, ForeignKey, Integer, Sequence, String, Table,
-    UniqueConstraint, Float
+    UniqueConstraint, Float, Boolean
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
@@ -13,11 +13,25 @@ from .utils import hash_password
 Base = declarative_base()
 
 
-class User(Base):
+class SerializeMixin(object):
+
+    serializeable = []
+
+    @property
+    def serialize(self):
+        return {
+            field: getattr(self, field, '')
+            for field in self.serializeable
+        }
+
+
+class User(Base, SerializeMixin):
 
     __tablename__ = 'users'
+    serializeable = ['id', 'username', 'fullname', 'serialize_punches']
 
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    administrator = Column(Boolean, default=False)
     username = Column(String)
     fullname = Column(String)
     password = Column(String)
@@ -54,6 +68,10 @@ class User(Base):
     def get_id(self):
         return str(self.id)
 
+    @property
+    def serialize_punches(self):
+        return [punch.serialize for punch in self.punches.all()]
+
 
 # Define punch-tag relationship table
 _punch_tags = Table(
@@ -63,9 +81,14 @@ _punch_tags = Table(
 )
 
 
-class Punch(Base):
+class Punch(Base, SerializeMixin):
 
     __tablename__ = 'punches'
+    serializeable = [
+        'id', 'start_time_string', 'end_time_string', 'description',
+        'total_time', 'serialize_tags'
+    ]
+
 
     id = Column(Integer, Sequence('punch_id_seq'), primary_key=True)
     start_time = Column(DateTime)
@@ -82,10 +105,23 @@ class Punch(Base):
             self.start_time, self.end_time, self.user_id
         )
 
+    @property
+    def start_time_string(self):
+        return self.start_time.isoformat()
 
-class Tag(Base):
+    @property
+    def end_time_string(self):
+        return self.end_time.isoformat() if self.end_time else ''
+
+    @property
+    def serialize_tags(self):
+        return [tag.serialize for tag in self.tags.all()]
+
+
+class Tag(Base, SerializeMixin):
 
     __tablename__ = 'tags'
+    serializeable = ['id', 'value']
 
     id = Column(Integer, Sequence('tag_id_seq'), primary_key=True)
     value = Column(String(50), nullable=False, unique=True)
